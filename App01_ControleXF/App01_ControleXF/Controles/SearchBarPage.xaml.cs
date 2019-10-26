5c8cf15b7990e6f023cd14cfb0285617c89c465b
Charlles <charlleslimamota10@hotmail.com>
2019-10-26 14:49:25 -0300

    Adicionar arquivos de projeto.

---
+++ App01_ControleXF/App01_ControleXF/Controles/SearchBarPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchBarPage : ContentPage
+    {
+        private List<String> conferenciaLeste;
+        public SearchBarPage()
+        {
+            InitializeComponent();
+
+            conferenciaLeste = new List<string>();
+            conferenciaLeste.Add("Celtics");
+            conferenciaLeste.Add("Knicks");
+            conferenciaLeste.Add("Nets");
+            conferenciaLeste.Add("Raptors");
+            conferenciaLeste.Add("Hawks");
+            conferenciaLeste.Add("Bucks");
+            conferenciaLeste.Add("Sixers");
+            conferenciaLeste.Add("Hornets");
+            conferenciaLeste.Add("Pacers");
+            conferenciaLeste.Add("Pistons");
+            conferenciaLeste.Add("Magic");
+            conferenciaLeste.Add("Heat");
+            conferenciaLeste.Add("Cavaliers");
+            conferenciaLeste.Add("Wizards");
+            conferenciaLeste.Add("Bulls");
+
+            Preencher(conferenciaLeste);            
+        }
+        private void Pesquisar(object sender, TextChangedEventArgs args)
+        {
+            var resultado = conferenciaLeste.Where(a => a.Contains(args.NewTextValue)).ToList<string>();
+
+            Preencher(resultado);
+        }
+        private void Preencher(List<String> conferenciaLeste)
+        {
+            ListResult.Children.Clear();
+            foreach (var times in conferenciaLeste)
+            {
+                ListResult.Children.Add(new Label { Text = times });
+            }
+        }
+    }
+}
---
+++ App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+        private void goActivityIndicatorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }
+        private void goProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgressBarPage();
+        }
+        private void goBoxViewPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.BoxViewPage();
+        }
+        private void goLabelPage (object sender, EventArgs args)
+        {
+            Detail = new Controles.LabelPage();
+        }
+        private void goButtonPage (object sender, EventArgs args)
+        {
+            Detail = new Controles.ButtonPage();
+        }
+        private void goEntryEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.EntryEditorPage();
+        }
+        private void goDatePickerEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.DatePickerPage();
+        }
+        private void goTimePickerEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.TimePickerPage();
+        }
+        private void goPickerEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.PickerPage();
+        }
+        private void goSearchBarEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.SearchBarPage();
+        }
+    }
+}
